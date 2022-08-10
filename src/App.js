f0d9e752395fd34ba9511e4026cff541dad8ce28
Michael An <1822852997@qq.com>
2022-08-10 08:27:26 +0800

    init repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Button } from 'reactstrap';
+import { Router, Link } from '@reach/router';
+import Demo from './pages/test1';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Chooser extends React.Component {
+
+  render() {
+    return (
+      <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div><h3>Please select the lib</h3></div>
+        <Link to='/demo-01'>
+          <Button className="btn btn-default">demo 01</Button>
+        </Link>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <Router primary={false} style={{display: 'flex', width: '100%', height: '100%'}}>
+        <Chooser path='/' />
+        <Demo path='/demo-01' />
+      </Router>
+    );
+  }
 }
 
 export default App;
